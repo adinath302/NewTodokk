abd4d2aa0d388eb3b5e157222c94b20b9ea21c7f
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-07-30 22:20:45 +0530

    Add files via upload

---
+++ UseReducer/UseReducer.jsx
@@ -0,0 +1,27 @@
+import React, { useReducer, useState } from 'react'
+
+const UseReducer = () => {
+
+    const reducer = (state, action) => {
+        if (action.type === "INCREMENT") {
+            return state + 1
+        }
+        if (action.type === "DECREMENT") {
+            return state - 1
+        }
+    }
+
+    // const [count, setcount] = useState(0)
+    const [count, dispatch] = useReducer(reducer, 0)
+
+    return (
+        <div className=" p-4 h-lvh flex flex-col justify-center
+items-center ">
+            <h1>{count}</h1>
+            <button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</button>
+            <button onClick={() => dispatch({ type: "DECREMENT" })}> Decrement</button>
+        </div >
+    )
+}
+
+export default UseReducer;
